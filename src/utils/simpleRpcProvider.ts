9fbe639fa74a487d710192ecfc1fa05a750a2d85
Austin McCall <85043551+DeathGamer111@users.noreply.github.com>
2023-03-04 07:27:42 -0600

    Update simpleRpcProvider.ts

--- src/utils/simpleRpcProvider.ts
+++ src/utils/simpleRpcProvider.ts
@@ -2,6 +2,6 @@ import { ethers } from "ethers";
 
 const RPC_URL = "https://rpc.giltestnet.com";
 
-const simpleRpcProvider = new ethers.providers.StaticJsonRpcProvider(RPC_URL, 56);
+const simpleRpcProvider = new ethers.providers.StaticJsonRpcProvider(RPC_URL, 4242);
 
 export default simpleRpcProvider;
