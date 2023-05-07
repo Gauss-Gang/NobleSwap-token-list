d8a1acc95837cccec18b9f1110b74f0870bbd8a4
Tenzija <milosmiskov.miki@gmail.com>
2023-05-07 22:04:32 +0200

    work

--- src/utils/simpleRpcProvider.ts
+++ src/utils/simpleRpcProvider.ts
@@ -2,6 +2,6 @@ import { ethers } from "ethers";
 
 const RPC_URL = "https://rpc.giltestnet.com";
 
-const simpleRpcProvider = new ethers.providers.StaticJsonRpcProvider(RPC_URL, 4242);
+const simpleRpcProvider = new ethers.providers.StaticJsonRpcProvider(RPC_URL, 1452);
 
 export default simpleRpcProvider;
